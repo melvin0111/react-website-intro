d8b60fb62abc9c6652e450e68d7390a246db7ffa
aazroui <aazroui@hawk.iit.edu>
2024-04-01 22:48:03 -0500

    fixed bug --> app.py

--- flask-live-crud/app.py
+++ flask-live-crud/app.py
@@ -4,53 +4,50 @@ from os import environ
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Taft#2021@flask_db:3306/sailing_club_database'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
 db = SQLAlchemy(app)
 
-class User(db.Model): 
+class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(80), unique = True, nullable = False)
-    email = db.Column(db.String(120), unique = True, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
-    def json(self): 
-        return {'id': id, 'username': self.username, 'email': self.email}
-    
-    db.create_all()
+    def json(self):
+        return {'id': self.id,'username': self.username, 'email': self.email}
 
-    #Create Test Route 
+db.create_all()
 
-    @app.route('/test', methods = ['GET'])
-    def test(): 
-        return make_response(jsonify({'message':  'test route'}), 200)
+#create a test route
+@app.route('/test', methods=['GET'])
+def test():
+  return make_response(jsonify({'message': 'test route'}), 200)
 
-# Create a User
 
-@app.route('/users')
-def create_user(): 
-    try:
-        data = request.get_json()
-        new_user = User(username= data)['username'], email = data['email']
-        db.session.add(new_user)
-        db.session.commit()
-        return make_response(jsonify({'message':  'user_created'}), 201)
-    except Exception as e: 
-        return make_response(jsonify({'message':  'error creating user'}), 500)
-    
-    # Get all Users
+# create a user
+@app.route('/users', methods=['POST'])
+def create_user():
+  try:
+    data = request.get_json()
+    new_user = User(username=data['username'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return make_response(jsonify({'message': 'user created'}), 201)
+  except e:
+    return make_response(jsonify({'message': 'error creating user'}), 500)
 
-@app.route('/users', methods = ['GET'])
-def get_users(): 
-    try: 
-        users = User.query.all()
-        return make_response(jsonify({'users': [user.json() for user in users]}), 200)
-    except: 
-        return make_response(jsonify({'message':  'error getting users'}), 500)
-    
+# get all users
+@app.route('/users', methods=['GET'])
+def get_users():
+  try:
+    users = User.query.all()
+    return make_response(jsonify([user.json() for user in users]), 200)
+  except e:
+    return make_response(jsonify({'message': 'error getting users'}), 500)
 
-# Get a User by ID
-    
+# get a user by id
 @app.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
   try:
@@ -58,11 +55,10 @@ def get_user(id):
     if user:
       return make_response(jsonify({'user': user.json()}), 200)
     return make_response(jsonify({'message': 'user not found'}), 404)
-  except:
+  except e:
     return make_response(jsonify({'message': 'error getting user'}), 500)
-  
-  # Update a User by ID
 
+# update a user
 @app.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
   try:
@@ -74,12 +70,10 @@ def update_user(id):
       db.session.commit()
       return make_response(jsonify({'message': 'user updated'}), 200)
     return make_response(jsonify({'message': 'user not found'}), 404)
-  except:
+  except e:
     return make_response(jsonify({'message': 'error updating user'}), 500)
-  
 
-# Delete a User
-  
+# delete a user
 @app.route('/users/<int:id>', methods=['DELETE'])
 def delete_user(id):
   try:
@@ -89,5 +83,5 @@ def delete_user(id):
       db.session.commit()
       return make_response(jsonify({'message': 'user deleted'}), 200)
     return make_response(jsonify({'message': 'user not found'}), 404)
-  except:
+  except e:
     return make_response(jsonify({'message': 'error deleting user'}), 500)
